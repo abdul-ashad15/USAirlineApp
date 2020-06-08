82bc71a8e628e236a1dbb6f43f790f537f87538b
abdul-ashad15 <abdul.ashad1@gmail.com>
2020-06-08 07:19:19 +0000

    code changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -52,5 +52,9 @@ export class LoginComponent implements OnInit {
       this.router.navigate(['/home']);
     }
     }
-  } 
+  }
+  register() : void
+  {
+    this.router.navigate(['/signup']);
+  }
 }
